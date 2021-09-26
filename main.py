2c675b95480be5ae24a27ecb489db46cbc84cc68
mbowen <mbowen775@g.rwu.edu>
2021-09-26 10:25:06 -0400

    Demo 2

--- main.py
+++ main.py
@@ -3,14 +3,23 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# This function asks the user's name and prints it to the screen
+def my_function():
+    # Get the user's name
+    name = input("Tell me your name:")
+    # Print the user's name
+    print(f'Hello{name}')
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')    #Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # print_hi('PyCharm')
+    my_function()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
